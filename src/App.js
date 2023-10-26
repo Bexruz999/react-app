c89105ae092b37f39b7a55e9327533de292c565f
Bexruz999 <bexruzfatullayev999@gmail.com>
2023-10-26 17:35:51 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { nanoid } from 'nanoid'
+import React, { useState } from 'react';
+
+function id() { return nanoid();}
 
 function App() {
-  const elem = <div><p>text</p></div>
-  return (
-    <div className="App">
+  const [name, setName] = useState(false);
+  const show = false;
+  function clickHandler() {
+    setName(!name);
+  }
+  let elem = '';
+  if (name) {
+    elem = <div><p>text</p></div>
+  } else {
+    elem = <div>{id()}</div>
+  }
+  return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           { elem }
         </a>
+        <button onClick={clickHandler}>btn</button>
       </header>
     </div>
-  );
 }
 
 export default App;
